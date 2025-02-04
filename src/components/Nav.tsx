1fcb72c6bfea9bce014e2f479bfcbdb9c08bb885
Aspen Zachman <azachman66@gmail.com>
2025-02-04 12:40:58 -0500

    Updated Nav.tsx to route to pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 const Nav = () => {
+  const navigate = useNavigate();
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
   <>
     <div className="nav">
       <div className="nav-item">
-        <div className="nav-link">
+        <div className="nav-link" onClick={() => navigate('/')}>
           Home
         </div>
       </div>
       <div className="nav-item">
-        <div className="nav-link">
+        <div className="nav-link" onClick={() => navigate('/CandidateSearch')}>
           Candidate Search
         </div>
       </div>
       <div className="nav-item">
-        <div className="nav-link">
+        <div className="nav-link" onClick={() => navigate('/SavedCandidates')}>
           Saved Candidates
         </div>
       </div>
