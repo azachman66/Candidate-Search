697fc752b8ab9c2a0176eff724042b7061166ffb
Aspen Zachman <azachman66@gmail.com>
2025-02-04 13:20:10 -0500

    CandidateSearch

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from "../interfaces/Candidate.interface.tsx";
 
 const CandidateSearch = () => {
+
+  const gitHub = searchGithub();
+  const githubUser = searchGithubUser(Candidate);
+
   return (<>
   <h1>Candidate Search</h1>
-  
+  <main>
+    <p>Name: {Candidate.username}</p>
+    <p>Location: {Candidate.location}</p>
+    <p>Email: {Candidate.email}</p>
+  </main>
   </>
   );
 };
